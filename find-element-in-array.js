35f793311a63a7945ce8a1cd35f1da27ac3192b5
Tom <tzchrzanowski@gmail.com>
2017-07-06 14:49:56 +0200

    Create find-element-in-array.js

---
+++ find-element-in-array.js
@@ -0,0 +1,6 @@
+[ 1, 3, 4, 2 ].find(x => x > 3) // 4
+[ 1, 3, 4, 2 ].findIndex(x => x > 3) // 2
+
+// es5
+// [ 1, 3, 4, 2 ].filter(function (x) { return x > 3; })[0]; // 4
+// cant find index this way
