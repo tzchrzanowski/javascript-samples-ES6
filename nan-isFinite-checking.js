566114232753b129bf72d2e239b043be0700229d
Tom <tzchrzanowski@gmail.com>
2017-07-06 14:57:39 +0200

    Create nan-isFinite-checking.js

---
+++ nan-isFinite-checking.js
@@ -0,0 +1,7 @@
+Number.isNaN(42) === false
+Number.isNaN(NaN) === true
+
+Number.isFinite(Infinity) === false
+Number.isFinite(-Infinity) === false
+Number.isFinite(NaN) === false
+Number.isFinite(123) === true
