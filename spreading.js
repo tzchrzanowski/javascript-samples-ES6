3620511d14936bbb62bd1757bb98f7f8d148ba9a
Tom <tzchrzanowski@gmail.com>
2017-07-06 11:58:29 +0200

    Create spreading.js

---
+++ spreading.js
@@ -0,0 +1,13 @@
+var params = [ "hello", true, 7 ]
+var other = [ 1, 2, ...params ] // [ 1, 2, "hello", true, 7 ]
+
+function f (x, y, ...a) {
+    return (x + y) * a.length
+}
+f(1, 2, ...params) === 9
+
+var str = "foo"
+var chars = [ ...str ] // [ "f", "o", "o" ] 
+
+// es5:
+// var chars = str.split(""); // [ "f", "o", "o" ]
