9b94cec39d451f7fc7b0fcbff2096b0022f1d9dc
Tom <tzchrzanowski@gmail.com>
2017-07-06 15:02:08 +0200

    Create number-trucnation.js

---
+++ number-trucnation.js
@@ -0,0 +1,11 @@
+console.log(Math.trunc(42.7)) // 42
+console.log(Math.trunc( 0.1)) // 0
+console.log(Math.trunc(-0.1)) // -0
+
+// es5
+// function mathTrunc (x) {
+//     return (x < 0 ? Math.ceil(x) : Math.floor(x))
+// }
+// console.log(mathTrunc(42.7)) // 42
+// console.log(mathTrunc( 0.1)) // 0
+// console.log(mathTrunc(-0.1)) // -0
